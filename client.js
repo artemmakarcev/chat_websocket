99a653bcebd1b732f6ed587f604418f66d0c5c72
artemmakarcev <makarcev4@gmail.com>
2023-09-20 17:07:47 +0500

    send all clients and save messages
    + Сохранение сообщений локально
    + Отправка сообщений по WS
    +Вывод сообщений новым пользователям

--- client.js
+++ client.js
@@ -2,9 +2,24 @@ const chatEl = document.getElementById("chat");
 const formEl = document.getElementById("messageForm");
 
 const ws = new WebSocket("ws://127.0.0.1:8000");
-ws.onmessage = (message) => {};
+ws.onmessage = (message) => {
+  console.log(JSON.parse(message.data));
+  const messages = JSON.parse(message.data);
+  messages.forEach((value) => {
+    const messageEl = document.createElement("div");
+    messageEl.appendChild(document.createTextNode(`${value.name}: ${value.message}`));
+    chatEl.appendChild(messageEl);
+  });
+};
+ws.onopen = ()=>{
+  console.log('ws open')
+}
+ws.onclose = () => {
+  console.log('ws close')
+};
 
 const send = (event) => {
+  event.preventDefault();
   const name = document.getElementById("name").value;
   const message = document.getElementById("message").value;
   ws.send(
--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-import { WebSocket } from "ws";
+import { WebSocketServer } from "ws";
 import { v4 as uuid } from "uuid";
+import { writeFileSync, readFileSync, existsSync } from "fs";
 const clients = {};
+const logFile = existsSync("log.json") && readFileSync("log.json");
+const messages = JSON.parse(logFile) || [];
 
-const wss = new WebSocket({ port: 8000 });
+const wss = new WebSocketServer({ port: 8000 });
 wss.on("connection", (ws) => {
   const id = uuid();
   clients[id] = ws;
 
   console.log("New client", id);
 
-  ws.on("message", (rawMessage) => {
-    console.log(rawMessage.data)
+  ws.send(JSON.stringify(messages));
+
+  ws.on("message", (data, isBinary) => {
+    console.log(JSON.parse(data), isBinary);
+    const { name, message } = JSON.parse(data);
+    messages.push({ name, message });
+    for (const id in clients) {
+      clients[id].send(JSON.stringify([{ name, message }]));
+    }
   });
   ws.on("close", () => {
     delete clients[id];
     console.log("client is closed", id);
   });
+  ws.on("error", console.error);
+});
+
+process.on("SIGINT", () => {
+  wss.close();
+  writeFileSync("log.json", JSON.stringify(messages), (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log('file save')
+  });
+  process.exit();
 });
